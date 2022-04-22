e03bb536d9a8961170c4a7eb61b15efb217085af
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2022-04-22 16:42:20 +0200

    tp8 Newton-Raphson changé pour vectoriel
    marche aussi en scalaire

--- BA2/ANANUM/tp7newtonraphson.m
+++ BA2/ANANUM/tp7newtonraphson.m
@@ -6,7 +6,8 @@ function [n, sol] = tp7newtonraphson(f, fprime, x0)
   #while (fprime(x) > 10^(-4)) & (fprime(x) < -10^(-4)) & n < 50
   while n < 69
     n++;
-    x = x - f(x)/fprime(x);
+    %x = x - f(x)/fprime(x); % si scalaire
+    x = x - fprime(x)\f(x); % pour vectoriel ET scalaire
   endwhile
   sol = x;
 endfunction
