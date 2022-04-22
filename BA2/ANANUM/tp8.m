8e5bde1e70718d6f5b0c4ef5a5b131bf0ae7a75c
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2022-04-22 17:40:21 +0200

    tp8

---
+++ BA2/ANANUM/tp8.m
@@ -0,0 +1,46 @@
+% TP8
+
+% appliquer Newton-Raphson à une focntion vecorielle
+% fct vectorielles:
+% F=@(x) [cos(x), sin(x), tan(x)]
+% F=@(x,y) [cos(x)+sin(y), x-y*cos(x)]
+% (on veut recycler ce qu'on a fait au tp précédant (?))
+% on peut aussi faire
+% F = @(x) [sin(x(1)), cos(x(2))+x(1)]  (ici x est un vecteur, ex: x=[1,3])
+% ce dernier est préferable pour réutiliser nos algo qui étaient scalaires à la base
+
+% la dérivée d'un fonction vectorielle est la matrice jacobienne
+% Fp = @(x) [DF_1/Dx_1, DF_2/Dx_2, DF_3/Dx_3]
+
+% on va devoir modifer une ligne de l'algo de Newton-Raphson:
+% x^(k+1) = x^(k) - f(x_k)/f'(x_k)
+
+% Ax=b  x=b/A  <=> x=A\b sur Octave
+% ligne 15 // //  - Fp(x)\F(x)   => va tout le temps fonctionner même si scalaire
+
+
+%fct du b)
+F=@(x) [x(1)^2-x(2)-1; (x(1)-2)^2+(x(2)-0.5)^2-1]
+
+%x = -2:0.1:2
+x = linspace(0, 2, 300);
+y = linspace(0, 2, 300);
+
+for i=1:size(x,2)
+  for j=1:size(y,2)
+    z(i,j)=norm(F([x(i),y(j)]));
+  endfor
+endfor
+mesh(x, y, z) % (set(gca,'Zlim',[-1,1]) dans command window)
+
+% on peut voir une racine en x=0.2, y=1.1
+% et l'autre en x=1.5, y=1.6
+
+
+% c)
+
+%jacobienne Fp
+Fp=@(x) [2*x(1), -1; 2*x(1)-4, 2*x(2)-1];
+x0 = [1.5; 1.6];
+%x0 = [0.2; 1.1];
+[n_nr , sol_nr] = tp7newtonraphson( F, Fp, x0)
