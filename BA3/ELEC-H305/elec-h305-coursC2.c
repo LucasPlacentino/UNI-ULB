763abb35af4934372c9db086bc2b7dece6ee215f
Lucas Placentino <23436953+lucasplacentino@users.noreply.github.com>
2022-10-23 12:58:07 +0200

    t

--- BA3/ELEC-H305/elec-h305-coursC2.c
+++ BA3/ELEC-H305/elec-h305-coursC2.c
@@ -1,28 +1,34 @@
 // COURS C n2 12/10/2022
 
+#include <stdio.h>
+
 void myFunc(int *x, int*y);
 int my_array[] = {1, 2, 56, 2, -4};
 int *ptr;
 
-main() {
+int main(void) {
     int n;
-    scanf("%d\n", &n);
-    printf("%d\n", n);
+    printf("Enter an int:\n");
+    scanf("%d", &n);
+    printf("Your value : %d\n", n);
 
     int *ip; // pointer
-    printf(%x, ip); // print the adress (in hex)
-    printf(%d, *ip); // print the value inside the adress
+    printf("ip (adress) : %x (in hex)\n", ip); // print the adress (in hex)
+    printf("*ip (value) : %d (in decimal)\n", *ip); // print the value inside the adress
 
-    int *p=7,*q=3;
+    int *p,*q;
+    *p = 7;
+    *q = 3;
     myFunc(p,q);
-    printf("Values: %d %d \n", *p, *q); // ? vérif
+    printf("Values of p and q: %d %d (in decimal)\n", *p, *q); // ? vérif
 
     ptr = &my_array[0];
-    for(i=0, i<6, i++) { // slide 26
+    int i;
+    for (i=0; i<6; i++) { // slide 26
         //printf("ptr + %d = %d \n", i, *)
         printf("ptr + %d = %d \n", i, *(ptr + i));
     }
-
+    return 0;
 }
 
 void myFunc(int *x, int*y) {
